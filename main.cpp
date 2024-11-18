f22426f31384252c4541c9c8bf25c546f654a81b
Hadi Chokr <hadichokr@icloud.com>
2024-11-18 14:42:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 
 double piformel(int iter) {
-   
     if (iter == 1) {
         return 1;
     }
-    
-    //iter war im nicht rekursiven Ansatz die jetzige iter aber in rekursion übrige iter
+
+    // iter war im nicht rekursiven Ansatz die jetzige Iteration, aber in der Rekursion die übrig gebliebenen Iterationen
     return piformel(iter - 1) + pow(-1, (iter - 1)) / (2.0 * (iter - 1) + 1);
 }
 
-int main()
-{
-    
-    double ergebnis = piformel(100) * 4;
-    
-    cout << ergebnis;
+int main() {
+    int iter = 0;
+
+    cout << "How many iterations to approximate pi do you want?" << endl;
+    cin >> iter;
+
+    double ergebnis = piformel(iter) * 4;
+    cout << "The result after " << iter << " iterations is: " << ergebnis << endl;
 
     return 0;
 }
